4d8791a4fa8e00fd10e8b4645eb1fd1a9ea6fadf
Marcel Streicher <marcel.streicher58@gmail.com>
2021-12-03 17:58:21 +0100

    Added spotipy-related functions

--- spotify_bot/bot.py
+++ spotify_bot/bot.py
@@ -5,7 +5,7 @@ import json
 from os import path
 
 
-class Bot(commands.Bot):
+class Bot():
     for location in path.expanduser("~/.config/spotify-bot/"), "/etc/spotify-bot/":
         try:
             print(path.join(location, "config.json"))
@@ -29,3 +29,49 @@ class Bot(commands.Bot):
                                                    client_secret=data['spotify']['spotify_client_secret'],
                                                    redirect_uri=data['spotify']['spotify_redirect_uri']))
 
+    def add_track_to_playlist(self, username, playlist_id, track_ids):
+        self.sp.trace = False
+        results = self.sp.playlist_add_items(username, playlist_id, track_ids)
+        print(results)
+        return 'Added track'
+
+    def get_current_track(self):
+        self.sp.trace = False
+        request = self.sp.current_user_playing_track()
+        if request is not None:
+            track = request['item']['name']
+            artist = request['item']['artists'][0]['name']
+            print(f'{track} - {artist}')
+            return f'{track} - {artist}'
+        else:
+            print('Nothing playing at the moment...')
+            return 'Nothing playing at the moment...'
+
+    def clear_playlist(self, username, playlist_id):
+        self.sp.trace = False
+        result = self.sp.playlist_tracks(playlist_id)
+        tracks = []
+        for i in result['items']:
+            tracks.append(i['track']['uri'])
+        results = self.sp.user_playlist_remove_all_occurrences_of_tracks(username, playlist_id, tracks)
+        print(results)
+        return 'Cleared playlist'
+
+    def skip_song(self):
+        self.sp.trace = False
+        for device in self.sp.devices()['devices']:
+            if device['is_active']:
+                results = self.sp.next_track(device_id=device['id'])
+                print(results)
+        return 'Skipped song'
+
+    def disable_loop_and_shuffle(self):
+        self.sp.trace = False
+        for device in self.sp.devices()['devices']:
+            if device['is_active']:
+                self.sp.shuffle(False, device['id'])
+                self.sp.repeat('off', device['id'])
+                return
+
+
+bot = Bot()
