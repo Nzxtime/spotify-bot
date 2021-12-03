c997d2e469d1071ab68a44fba3dfdd39b5bb0620
Marcel Streicher <marcel.streicher58@gmail.com>
2021-12-03 18:49:14 +0100

    Added twitchio related functions

--- spotify_bot/bot.py
+++ spotify_bot/bot.py
@@ -5,7 +5,7 @@ import json
 from os import path
 
 
-class Bot():
+class Bot(commands.Bot):
     for location in path.expanduser("~/.config/spotify-bot/"), "/etc/spotify-bot/":
         try:
             print(path.join(location, "config.json"))
@@ -29,9 +29,9 @@ class Bot():
                                                    client_secret=data['spotify']['spotify_client_secret'],
                                                    redirect_uri=data['spotify']['spotify_redirect_uri']))
 
-    def add_track_to_playlist(self, username, playlist_id, track_ids):
+    def add_track_to_playlist(self, playlist_id, track_ids):
         self.sp.trace = False
-        results = self.sp.playlist_add_items(username, playlist_id, track_ids)
+        results = self.sp.playlist_add_items(playlist_id, track_ids)
         print(results)
         return 'Added track'
 
@@ -73,5 +73,57 @@ class Bot():
                 self.sp.repeat('off', device['id'])
                 return
 
+    def __init__(self):
+        super().__init__(token=self.data['twitch']['twitch_access_token'],
+                         prefix=self.data['twitch']['twitch_prefix'],
+                         initial_channels=self.data['twitch']['twitch_initial_channels'])
+
+    async def event_ready(self):
+        print(f'Ready | {self.nick}')
+
+    async def event_message(self, message):
+        print(message.content)
+        await self.handle_commands(message)
+
+    @commands.command(name='song')
+    async def song(self, ctx):
+        result = self.get_current_track()
+        await ctx.send(f'@{ctx.author.name} {result}')
+
+    @commands.command(name='play')
+    async def play(self, ctx, arg1):
+        result = self.add_track_to_playlist(self.data['spotify']['spotify_playlist_id'], [arg1])
+        await ctx.send(f'@{ctx.author.name} {result}')
+
+    @commands.command(name='skip')
+    async def skip(self, ctx):
+        if ctx.author.is_mod or str(ctx.author.name).lower() == str(ctx.message.channel).lower():
+            result = self.skip_song()
+            await ctx.send(f'@{ctx.author.name} {result}')
+        else:
+            await ctx.send(f'@{ctx.author.name}, you don\'t have the needed permission!')
+
+    @commands.command(name='clear')
+    async def clear(self, ctx):
+        if ctx.author.is_mod or str(ctx.author.name).lower() == str(ctx.message.channel).lower():
+            result = self.clear_playlist(self.data['spotify']['spotify_username'],
+                                         self.data['spotify']['spotify_playlist_id'])
+            await ctx.send(f'@{ctx.author.name} {result}')
+        else:
+            await ctx.send(f'@{ctx.author.name}, you don\'t have the needed permission!')
+
+    @commands.command(name='dls')
+    async def dls(self, ctx):
+        if ctx.author.is_mod or str(ctx.author.name).lower() == str(ctx.message.channel).lower():
+            self.disable_loop_and_shuffle()
+            await ctx.send(f'@{ctx.author.name} disabled loop and shuffle!')
+        else:
+            await ctx.send(f'@{ctx.author.name}, you don\'t have the needed permission!')
+
+    @commands.command(name='help')
+    async def dls(self, ctx):
+        await ctx.send(f'@{ctx.author.name} song, play, skip, clear, dls')
+
 
 bot = Bot()
+bot.run()
