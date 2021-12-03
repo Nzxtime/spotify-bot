51b133f86ab80edcd8757e8c59a372762ef4be92
Marcel Streicher <marcel.streicher58@gmail.com>
2021-12-03 17:20:45 +0100

    Added first files

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="spotify-bot",
+    version="2.0.0",
+    author="Nzxtime",
+    author_email="marcel.streicher58@gmail.com",
+    description="A twitch bot which can control your spotify account",
+    long_description="A twitch bot which can control your spotify account. It can queue songs to a specific playlist, skip songs on request, return your current song and many more.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nzxtime/spotify-bot",
+    python_requires='>=3.6',
+)
